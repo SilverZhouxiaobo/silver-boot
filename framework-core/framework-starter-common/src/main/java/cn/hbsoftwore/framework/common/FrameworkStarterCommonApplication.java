05457312173af4302f0647c718f339068fb4524e
huanghaipi <1552297011@qq.com>
2021-02-20 16:26:56 +0800

    init

---
+++ framework-core/framework-starter-common/src/main/java/cn/hbsoftwore/framework/common/FrameworkStarterCommonApplication.java
@@ -0,0 +1,13 @@
+package cn.hbsoftwore.framework.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkStarterCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkStarterCommonApplication.class, args);
+	}
+
+}
---
+++ framework-core/framework-starter-common/src/test/java/cn/hbsoftwore/framework/common/FrameworkStarterCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hbsoftwore.framework.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkStarterCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
