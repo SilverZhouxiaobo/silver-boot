885eebbd3ca3fe801bcd28abeb7a59d3f2298e2e
zhouxiaobo <silver.zhou@aliyun.com>
2023-06-29 17:38:15 +0800

    项目结构调整

---
+++ silver-framework/silver-boot-starter-webflux/src/main/java/cn/silver/boot/framework/controller/BaseController.java
@@ -0,0 +1,10 @@
+package cn.silver.boot.framework.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public abstract class BaseController {
+
+}
